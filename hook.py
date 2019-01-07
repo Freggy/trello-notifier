627ed7d2dee6256e69474411353878139028d755
Yannic <riegeryannic@gmail.com>
2019-01-07 20:14:47 +0100

    Use environment variables to get API keys and other information

--- hook.py
+++ hook.py
@@ -1,13 +1,21 @@
+import os
 from discord import Discord
 from flask import Flask, request
 from trello import TrelloClient
 
 
+DISCORD_API_TOKEN   = os.environ.get('DISCORD_API_TOKEN')
+DISCORD_CHANNEL_ID  = os.environ.get('DISCORD_CHANNEL_ID')
+TRELLO_API_KEY      = os.environ.get('TRELLO_API_KEY')
+TRELLO_API_SECRET   = os.environ.get('TRELLO_API_SECRET')
+TRELLO_API_TOKEN    = os.environ.get('TRELLO_API_TOKEN')
+MODEL_ID            = os.environ.get('MODEL_ID')
+
 app = Flask(__name__)
-discord = Discord('DISCORD_TOKEN', 'CHANNEL_ID')
-trello_client = TrelloClient(api_key='TRELLO_API_KEY',
-                             api_secret='TRELLO_API_SECRET',
-                             token='TRELLO_TOKEN')
+discord = Discord(DISCORD_API_TOKEN, DISCORD_CHANNEL_ID)
+trello_client = TrelloClient(api_key=TRELLO_API_KEY,
+                             api_secret=TRELLO_API_SECRET,
+                             token=MODEL_ID)
 
 template = """
     **Card ID:** {} \n 
@@ -36,12 +44,11 @@ def get_assignees(member_ids):
     assignees = []
     for id in member_ids:
         assignees.append('@' + trello_client.get_member(id).full_name)
-
     if assignees: # List is not empty
         return ', '.join(str(a) for a in assignees)
     return " "
 
 
 if __name__ == '__main__':
-    trello_client.create_hook('CALLBACK_URL', 'MODEL_ID')
+    trello_client.create_hook(TRELLO_API_KEY, MODEL_ID)
     app.run(host="0.0.0.0", port=1337)
